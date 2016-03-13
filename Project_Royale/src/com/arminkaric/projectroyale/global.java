225cecc35d870983b5dffadde3513153329093c8
Armin Karic <arminko2003@gmail.com>
2016-03-13 14:00:31 -0400

    Created global.java, and main_filehandler.java

---
+++ Project_Royale/src/com/arminkaric/projectroyale/global.java
@@ -0,0 +1,25 @@
+/**
+ * This class was created to hold methods, int's, etc. that all classes would use.
+ */
+package com.arminkaric.projectroyale;
+
+/**
+ * @author armin
+ *
+ */
+
+public class global {
+	public static final String clear = null;
+//Methods
+	//Clears all text
+	public void clear(){
+		int i = 0;
+		while(i != 75){
+			System.out.println();i++;
+			}
+	}
+	
+}
+	
+
+
---
+++ Project_Royale/src/com/arminkaric/projectroyale/main_filehandler.java
@@ -0,0 +1,12 @@
+/**
+ * Class handles the loading and saving of files.
+ */
+package com.arminkaric.projectroyale;
+
+/**
+ * @author arminkaric
+ *
+ */
+public class main_filehandler {
+
+}
