0d3646e68fdb8e3e945cd9391989e0a844cd7c67
Armin Karic <arminko2003@gmail.com>
2016-03-13 15:26:51 -0400

    Correctly Formated Version in package-info.

--- Project_Royale/src/com/arminkaric/projectroyale/package-info.java
+++ Project_Royale/src/com/arminkaric/projectroyale/package-info.java
@@ -1,8 +1,14 @@
 /**
  * Copyright Armin Karic 2016
+ * 
+ * Version Formatting
+ * MAJOR.MINOR.PATCH
+ * MAJOR version when you make incompatible API changes,
+ * MINOR version when you add functionality in a backwards-compatible manner, and
+ * PATCH version when you make backwards-compatible bug fixes.
  */
 /**
  * @author arminkaric
- * 
+ * @version 1.9.2
  */
 package com.arminkaric.projectroyale;
