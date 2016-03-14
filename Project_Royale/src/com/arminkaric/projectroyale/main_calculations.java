6947eeb402fa09151f0237ce7f70d01304621c01
Armin Karic <arminko2003@gmail.com>
2016-03-14 17:01:32 -0400

    Fixed Conflict Issue

--- Project_Royale/src/com/arminkaric/projectroyale/main_calculations.java
+++ Project_Royale/src/com/arminkaric/projectroyale/main_calculations.java
@@ -262,24 +262,22 @@ public String[] chestindex = {
 
 //Prediction Functions
 	//Calibrates the system if chests have been skipped
-		public void ifchestchestskiclear(){
-			g.clear();
-			System.output.println(g.ANSI_RED + "Warning, this isn't the most accurate prediction, it's either on the spot, or completely off. Regularly calibrate to get this as accurate as possible.");
-
+		public void ifchestchestskipped(){
+			
 		}
 	
 	//No skipped Chest
 	 public void chestprediction () throws InterruptedException{
 		 g.clear();
 			//Win Input
-		System.out.println("Current Wins:");
+		System.out.println(g.ANSI_YELLOW + "Current Wins:");
 		Scanner r = new Scanner(System.in);
 		int w = r.nextInt();
 			// Confirmation 
 		h++;
-		System.out.print("Chest just Recived ");System.out.printf(chestindex[w]);
+		System.out.print(g.ANSI_YELLOW+"Chest just Recived "+g.ANSI_RESET);System.out.printf(g.ANSI_RED+chestindex[w]);
 		System.out.println();
-		System.out.println("Y/N:");
+		System.out.println(g.ANSI_YELLOW+"Y/N:");
 		Scanner r1 = new Scanner(System.in);
 		String x = r1.next();
 			
@@ -300,16 +298,19 @@ public String[] chestindex = {
 				//Debug System.out.println(chestindex[y]);
 				y++;
 			}
-			int g = t-w;
+			int gl = t-w;
 			int m = y-w;
 			//Final Output
 			System.out.println("Assuming a chest is never skipped,");
-			System.out.printf("Next Chest is, ");System.out.printf(chestindex[e]);
+			System.out.printf(g.ANSI_YELLOW+"Next Chest is, ");System.out.printf(g.ANSI_RED+chestindex[e]);
 			System.out.println();
-			System.out.printf("Next Gold Chest is in, ");System.out.print(g);System.out.printf(" Wins ");
+			System.out.printf(g.ANSI_YELLOW+"Next Gold Chest is in, ");System.out.print(g.ANSI_RED+gl);System.out.printf(g.ANSI_YELLOW+" Wins ");
 			System.out.println();
-			System.out.printf("Next Magic Chest is in, ");System.out.print(m);System.out.printf(" Wins ");
+			System.out.printf(g.ANSI_YELLOW+"Next Magic Chest is in, ");System.out.print(g.ANSI_RED+m);System.out.printf(g.ANSI_YELLOW+" Wins ");
 			System.out.println();
+			Thread.sleep(10000);
+			System.exit(0);
+			
 
 			
 		}
@@ -323,21 +324,21 @@ public String[] chestindex = {
 			if(h == 3){
 				g.clear();
 				//Confirmation
-		System.out.println("Are you SURE you typed the correct amount of wins? (You Typed: " + w +")");
+		System.out.println(g.ANSI_RED+"Are you SURE you typed the correct amount of wins? (You Typed: " + w +")");
 		Scanner r11 = new Scanner(System.in);
 			System.out.println("Y/N:");
 			String x1 = r11.next();
 				if(x1.equals("Y") || x1.equals("y")){
-						System.out.println("Are you SURE you never played a game (and won) W/O a open chest slot?");
+						System.out.println(g.ANSI_RED+"Are you SURE you never played a game (and won) W/O a open chest slot?");
 					Scanner r111 = new Scanner(System.in);
 						System.out.println("Y/N");
 						String x11 = r111.next();
 								if(x11.equals("Y") || x11.equals("y")){
-							System.out.println("Please check your infomation once more.");
+							System.out.println(g.ANSI_YELLOW+"Please check your infomation once more.");
 							chestprediction();
 					}
 								if(x11.equals("N") || x11.equals("n")){
-							System.out.println("Please calibrate the system.");
+							System.out.println(g.ANSI_YELLOW+"Please calibrate the system."+g.ANSI_RESET);
 							Thread.sleep(3000);
 								System.exit(0);
 							
